31431e520965a98f39e4ea84b6f0c2347cf90954
shaddy104 <shaddy104@gmail.com>
2022-10-30 21:43:50 +0530

    new files

--- Django/new_arrival_dev/myworld/members/views.py
+++ Django/new_arrival_dev/myworld/members/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 
 # Create your views here.
+#def index(request):
+#    template = loader.get_template('myfirst.html')
+#    return HttpResponse(template.render())
+def index(request):
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+    return HttpResponse(output)
