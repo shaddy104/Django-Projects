3902df0e584f469fb6ea47cf8a87372202f00c59
Shadrach Pullikandla <shaddy104@gmail.com>
2022-10-31 11:13:55 +0530

    added new features

--- Django/new_arrival_dev/myworld/members/views.py
+++ Django/new_arrival_dev/myworld/members/views.py
@@ -1,15 +1,35 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 
 # Create your views here.
 #def index(request):
 #    template = loader.get_template('myfirst.html')
 #    return HttpResponse(template.render())
-def index(request):
+
+"""def index(request):
     mymembers = Members.objects.all().values()
     output = ""
     for x in mymembers:
         output += x["firstname"]
-    return HttpResponse(output)
+    return HttpResponse(output)"""
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
