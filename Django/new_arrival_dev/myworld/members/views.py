f02e7787d3e15a3bd8c1642ee30191d2711e1d7e
Shadrach Pullikandla <shaddy104@gmail.com>
2022-11-02 13:17:38 +0530

    New Features

--- Django/new_arrival_dev/myworld/members/views.py
+++ Django/new_arrival_dev/myworld/members/views.py
@@ -33,3 +33,25 @@ def addrecord(request):
     member = Members(firstname=x, lastname=y)
     member.save()
     return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
